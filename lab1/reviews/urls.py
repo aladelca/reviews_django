baba79d4c3c4d61ba3a7575de104bf76adf1b4a3
Adrian Alarcon <alarcon.adrianc@gmail.com>
2025-09-27 18:08:33 -0400

    last version

--- lab1/reviews/urls.py
+++ lab1/reviews/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('list/', views.list_review, name = 'list_review'),
+    path('add/', views.add_review, name = 'add_review'),
 ]
--- lab1/reviews/views.py
+++ lab1/reviews/views.py
@@ -3,7 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Review
+from .forms import ReviewForm
+from django.shortcuts import redirect
 
 def index(request):
     params = {}
     return render(request, "index.html", params)
+
+def list_review(request):
+    reviews = Review.objects.all()
+    params = {"reviews" : reviews}
+    return render(request, "list_review.html", params)
+
+def add_review(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ReviewForm()
+    return render(request, 'add_review.html', {'form': form})
